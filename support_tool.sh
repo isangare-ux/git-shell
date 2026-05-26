db1b5c5834f5437610f78bcd30326f218c7ca235
Ibrahim Sangaré <1234567+isangare-ux@users.noreply.github.com>
2026-05-26 16:53:30 +0200

    Menue mit case ausgewertet

--- support_tool.sh
+++ support_tool.sh
@@ -12,3 +12,20 @@ echo "1) Systeminformationen anzeigen"
 echo "2) Speicherplatz prüfen"
 echo "3) Programm beenden"
 read -p "Ihre Auswahl:" auswahl
+
+case $auswahl in
+  1)
+    echo "Systeminformationen:"
+    uname -a
+    ;;
+  2)
+    echo "Speicherplatz:"
+    df -h
+    ;;
+  3)
+    echo "Programm wird beendet."
+    ;;
+  *)
+    echo "Ungültige Auswahl."
+    ;;
+esac
