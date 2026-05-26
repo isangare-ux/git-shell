7ed0f907e609893b4b9abcac3145a85a659ce9cc
Ibrahim Sangaré <1234567+isangare-ux@users.noreply.github.com>
2026-05-26 16:41:53 +0200

    Einfaches Menue hinzugefügt

--- support_tool.sh
+++ support_tool.sh
@@ -6,4 +6,9 @@ echo "Dieses Skript hilft bei einfachen Support-Aufgaben."
 echo "Bitte geben Sie Ihren Namen ein:"
 read name
 echo "Hallo $name, willkommen im IT Support Tool."
-
+echo ""
+echo "Bitte wählen Sie eine Aufgabe:"
+echo "1) Systeminformationen anzeigen"
+echo "2) Speicherplatz prüfen"
+echo "3) Programm beenden"
+read -p "Ihre Auswahl:" auswahl
